179bd2c334b3f97c19fd5c8b33b3e8bb18df0376
Thiago <Thiagodacostaalves35@gmail.com>
2026-05-20 00:51:54 -0300

    feat: implement question repository and mongoose schema

--- src/modules/Question/domain/question.repository.ts
+++ src/modules/Question/domain/question.repository.ts
@@ -1,3 +1,11 @@
-export interface IRepository {
-    
+import { IRepository } from "src/common/domain/repository/IRepository";
+import { Question } from "../domain/question.model";
+
+export type createQuestionProps = Omit<
+  Question,
+  "id" | "createdAt" | "updatedAt"
+>;
+
+export interface IQuestionRepository extends IRepository<Question, Question> {
+  findByCategory(category: string): Promise<Question[]>;
 }
---
+++ src/modules/Question/infra/mongoose/repository/question-mongoose.repository.ts
@@ -0,0 +1,3 @@
+import { IQuestionRepository } from "src/modules/Question/domain/question.repository";
+
+export class QuestionMongooseRepository implements IQuestionRepository {}
